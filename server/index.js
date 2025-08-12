143e9a1356171c6f96ced0c4d9086887b721fc43
Mahithasukumara <mahissk2005@gmail.com>
2025-08-12 22:10:59 +0530

    added

--- server/index.js
+++ server/index.js
@@ -29,11 +29,33 @@ const createWorker=async()=>{
             process.exit();
         },3000)
     })
-    console.log(`Mediasoup worker created ${newWorker.id}`)
-    return newworker;
+    console.log(`Mediasoup worker created ${newWorker.pid}`)
+    return newWorker;
 
 }
 createWorker()
+const mediaCodecs=[{
+    kind:"audio",
+    mimeType:"audio/opus",
+    clockRate:48000,
+    channels:2,
+    preferredPayLoadType:98,
+    rtcpFeedback:{
+        type:"nack"
+    }
+
+},{
+    kind:"video",
+    mimeType:"video/vp8",
+    clockRate:90000,
+    parameters:{
+        "x-google-start-bitrate":2000
+    },
+    rtcpFeedback:{
+        type:"nack"
+    }
+    
+}]
 
 server.listen(5000,()=>{
     console.log("Server running in port 8000");
